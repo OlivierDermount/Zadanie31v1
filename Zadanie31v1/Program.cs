12745a3b6a5cf1abc1426538a3af658df9a6edc8
olivierdermount <olivierdermount@gmail.com>
2021-01-13 23:32:00 +0100

    Update

---
+++ Zadanie31v1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie31v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var writeReadDataAndCalculation = new WriteReadDataAndCalculation();
+                try
+                {
+                    var name = writeReadDataAndCalculation.WriteScrAndReadKey("Cześć! Podaj imię");
+
+                    var dateOfBirth = writeReadDataAndCalculation.WriteScrAndReadKey(
+                        "Możesz podać cyframi datę urodzenia\n" +
+                        " (w formacie rrrr.mm.dd)");
+
+                    var age = writeReadDataAndCalculation.AgeVeryficationAndCalculation(dateOfBirth);
+
+                    var exit = writeReadDataAndCalculation.WriteScrAndReadKey(name + " masz lat " + age + "\n" +
+                        "\n Czy chcesz wprowadzić nowych użytkowników?  t/n");
+                    if (exit == "n") break;
+                }
+                catch (Exception ex)
+                {
+                    writeReadDataAndCalculation.WriteScrAndReadKey(ex.Message + "\n Wciśnij Enter...");
+                }
+            }
+        }
+    }
+}
---
+++ Zadanie31v1/WriteReadDataAndCalculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie31v1
+{
+    class WriteReadDataAndCalculation
+    {
+        public string WriteScrAndReadKey(string Text)
+        {
+            Console.WriteLine(Text);
+            return Console.ReadLine();
+        }
+
+        public int AgeVeryficationAndCalculation(string dateOfBirth)
+        {
+            DateTime date = DateTime.ParseExact(dateOfBirth, "yyyy.MM.dd",
+                                System.Globalization.CultureInfo.InvariantCulture);
+            var year = date.Year;
+            var month = date.Month;
+            var todayDate = DateTime.Now;
+            var numberOfMonth = month - todayDate.Month;
+            var ageInt = (numberOfMonth <= 0) ?
+                todayDate.Year - year : todayDate.Year - year - 1;
+            return ageInt;
+        }
+    }
+}
